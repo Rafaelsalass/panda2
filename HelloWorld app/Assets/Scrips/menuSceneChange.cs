83d63529d185d4899eed60e71f5b7f1e5059124b
Rafael Salas <rafaelsalass20@gmail.com>
2017-04-03 00:29:30 -0400

    menu almost done

---
+++ HelloWorld app/Assets/Scrips/menuSceneChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuSceneChange : MonoBehaviour {
+    public int level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeScene(){
+        switch (level) {
+        case 1:
+            SceneManager.LoadScene ("level1");
+            break;
+        case 2:
+            SceneManager.LoadScene ("level2");
+            break;
+        case 3:
+            SceneManager.LoadScene ("level3");
+            break;
+        case 4:
+            SceneManager.LoadScene ("level4");
+            break;
+        case 5:
+            SceneManager.LoadScene ("Menu");
+            break;
+        }
+    }
+}
